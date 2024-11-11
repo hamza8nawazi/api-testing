276ef7b6521dc90d586a77792c405053468c7d02
hamza8nawazi <barkatullahnawazih@gmail.com>
2024-11-11 13:08:26 +0500

    .

---
+++ api.js
@@ -0,0 +1,16 @@
+const apiUrl = "https://jsonplaceholder.typicode.com/posts";
+
+fetch(apiUrl)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
